e71b8e5ce43c49d63e0ff035ae6ca17fd038e8ff
Kresna Sucandra <kresnasucandra@gmail.com>
2022-12-23 10:43:30 +0800

    type annotations: array and class

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -12,3 +12,14 @@ let nothing: undefined = undefined;
 
 // Build in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {
+
+}
+let car: Car = new Car();
